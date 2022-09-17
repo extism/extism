9e385386e38bab0935d4b94e5c1532f780457a9b
zach <zachshipko@gmail.com>
2022-09-17 10:21:55 -0700

    fix: haskell example

--- haskell/Example.hs
+++ haskell/Example.hs
@@ -7,11 +7,16 @@ import Extism.Manifest
 
 main = do
   plugin <- Extism.registerManifest (manifest [wasmFile "../wasm/code.wasm"]) False
-  res <- Extism.call plugin "count_vowels" (Extism.toByteString "this is a test")
-  case res of
+  case plugin of
     Right (Error msg) -> do
       _ <- putStrLn msg
       exitFailure
-    Left bs -> do
-      _ <- putStrLn (Extism.fromByteString bs)
-      exitSuccess
+    Left plugin -> do
+      res <- Extism.call plugin "count_vowels" (Extism.toByteString "this is a test")
+      case res of
+        Right (Error msg) -> do
+          _ <- putStrLn msg
+          exitFailure
+        Left bs -> do
+          _ <- putStrLn (Extism.fromByteString bs)
+          exitSuccess
