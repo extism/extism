7815d9aee6bb1d2e9403eff4f1675cc3fe29ae86
Benjamin Eckel <bhelx@simst.im>
2022-10-20 17:04:26 -0500

    Fix elixir publish pipeline

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -4,10 +4,11 @@ defmodule Extism.MixProject do
   def project do
     [
       app: :extism,
-      version: "0.0.1-rc.4",
+      version: "0.0.1-rc.5",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: package(),
       aliases: aliases()
     ]
   end
@@ -23,6 +24,7 @@ defmodule Extism.MixProject do
     [
       {:rustler, "~> 0.26.0"},
       {:json, "~> 1.4"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
     ]
   end
 
@@ -32,6 +34,16 @@ defmodule Extism.MixProject do
         "format",
         "cmd cargo fmt --manifest-path native/io/Cargo.toml"
       ]
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["BSD-3-Clause"],
+      description: "Extism Host SDK for Elixir and Erlang",
+      name: "extism",
+      files: ~w(lib native priv .formatter.exs mix.exs README.md LICENSE),
+      links: %{ "GitHub" => "https://github.com/extism/extism" },
     ]
   end
 end
