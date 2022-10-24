8724de8667cfaf3886da346d23b7449c0b791f6d
Benjamin Eckel <bhelx@users.noreply.github.com>
2022-10-24 14:00:26 -0500

    docs(ruby): fix example.rb context usage (#40)

--- ruby/example.rb
+++ ruby/example.rb
@@ -3,8 +3,9 @@ require 'json'
 
 # a Context provides a scope for plugins to be managed within. creating multiple contexts
 # is expected and groups plugins based on source/tenant/lifetime etc.
-ctx = Extism::Context.new
-Extism::with_context {|ctx| 
+# We recommend you use `Extism.with_context` unless you have a reason to keep your context around.
+# If you do you can create a context with `Extism#new`, example: `ctx = Extism.new`
+Extism.with_context do |ctx| 
   manifest = {
     :wasm => [{:path => "../wasm/code.wasm"}]
   }
@@ -13,4 +14,5 @@ Extism::with_context {|ctx|
   res = JSON.parse(plugin.call("count_vowels", ARGV[0] || "this is a test"))
   
   puts res['count']
-}
+end
+
