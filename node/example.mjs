fe383952366a9dae54cf01982683655e0a03cb8c
Benjamin Eckel <bhelx@users.noreply.github.com>
2022-10-27 17:22:05 -0500

    docs(node-sdk): Add docs, Makefile, format code (#55)

--- node/example.mjs
+++ node/example.mjs
@@ -1,22 +1,22 @@
-import { withContext, Context } from './dist/index.js';
-import { readFileSync } from 'fs';
+import { withContext, Context } from "./dist/index.js";
+import { readFileSync } from "fs";
 
 withContext(async function (context) {
-  let wasm = readFileSync('../wasm/code.wasm');
+  let wasm = readFileSync("../wasm/code.wasm");
   let p = context.plugin(wasm);
 
-  if (!p.functionExists('count_vowels')) {
+  if (!p.functionExists("count_vowels")) {
     console.log("no function 'count_vowels' in wasm");
     process.exit(1);
   }
 
-  let buf = await p.call('count_vowels', process.argv[2] || 'this is a test');
-  console.log(JSON.parse(buf.toString())['count']);
+  let buf = await p.call("count_vowels", process.argv[2] || "this is a test");
+  console.log(JSON.parse(buf.toString())["count"]);
   p.free();
 });
 
 // or, use a context like this:
 let ctx = new Context();
-let wasm = readFileSync('../wasm/code.wasm');
+let wasm = readFileSync("../wasm/code.wasm");
 let p = ctx.plugin(wasm);
-// ... where the context can be passed around to various functions etc. 
+// ... where the context can be passed around to various functions etc.
