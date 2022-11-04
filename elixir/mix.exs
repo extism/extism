17a1ca674e53be3a402b0cd7f6f51732cbc9ef0a
Benjamin Eckel <bhelx@simst.im>
2022-11-04 14:09:40 -0500

    Bump to version 0.0.1-rc.6

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Extism.MixProject do
   def project do
     [
       app: :extism,
-      version: "0.0.1-rc.5",
+      version: "0.0.1-rc.6",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
--- ruby/lib/extism/version.rb
+++ ruby/lib/extism/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Extism
-  VERSION = "0.0.1.rc4"
+  VERSION = "0.0.1.rc6"
 end
