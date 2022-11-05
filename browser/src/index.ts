7b53e79774b2703719ac28d459fd24ca2f8dc9db
zach <zachshipko@gmail.com>
2022-11-05 11:05:49 -0700

    feat: add allocator

--- browser/src/index.ts
+++ browser/src/index.ts
@@ -1,34 +1,92 @@
-
-function unpackU64toU8(n: BigInt): Uint8Array {
-    //@ts-ignore
-    return new Uint8Array(new BigInt64Array([n]).buffer)
+function stringToBytes(s: string, offset: number, len: number): Uint8Array {
+    return new Uint8Array(s.slice(offset, len).split("").map(c => c.charCodeAt(0)))
 }
 
-function packU8toU64(bytes: Uint8Array): BigInt {
-    return (new BigInt64Array(bytes.buffer))[0]
+type MemoryBlock = {offset: BigInt, length: BigInt}
+
+class Allocator {
+    currentIndex: BigInt
+    active: Record<BigInt, MemoryBlock>
+    freed: MemoryBlock[]
+    memory: Uint8Array
+    
+    constructor() {
+        this.currentIndex = BigInt(1);
+        this.active = {};
+        this.freed = [];    
+        this.memory = new Uint8Array(1024)
+    }
+    
+    alloc(length: BigInt) : MemoryBlock {
+        for (var block of this.freed) {
+            if (block.length == length){
+                this.active[block.offset] = block;
+                return block.offset;    
+            } else if (block.length > length + BigInt(64)){
+                const newBlock = {offset: block.offset, length};
+                block.offset += length;
+                block.length -= length;
+                return newBlock.offset;
+            }
+        }
+        
+        // Resize memory if needed
+        // TODO: put a limit on the memory size
+        if (BigInt(this.memory.length) < this.currentIndex + length){
+            const tmp = new Uint8Array(this.currentIndex + length + BigInt(64)).set(this.memory.buffer);
+            this.memory = tmp;
+        }
+        
+        const offset = this.currentIndex;
+        this.currentIndex += length;
+        this.active[offset] = {offset, length};
+        return this.active[offset];
+    }
+    
+    getLength(offset: BigInt) : BigInt {
+        const block = this.active[offset];
+        if (!block){
+            return 0;    
+        }
+        
+        return block.length;
+    }
+    
+    free(offset: BigInt) {
+        const block = this.active[offset];
+        if (!block){
+            return;    
+        }
+        
+        delete this.active[offset];
+        this.freed.append(block);
+    }
 }
 
-function makeEnv(plugin: ExtismPluginCall): any {
+function makeEnv(plugin: ExtismPlugin): any {
     return {
         extism_alloc(n: BigInt): BigInt {
-            return BigInt(10000) // just pick a safe? number for now
+            return plugin.allocator.alloc(n).offset;
         },
         extism_free (n: BigInt) {
-            console.log("Free ", n)
+            plugin.allocator.free(n);
         },
-        extism_load_u8(n: BigInt): number { debugger; return 0 },
+        extism_load_u8(n: BigInt): number { 
+            return plugin.allocator.memory[Number(n)];
+        },
         extism_load_u32(n: BigInt): number {debugger; return 0 },
-        extism_load_u64(n: BigInt): BigInt { debugger;return BigInt(0)},
+        extism_load_u64(n: BigInt): BigInt { 
+            let cast = new BigUint64Array(plugin.allocator.memory.buffer, Number(n) - 1);
+            return cast[0];
+        },
         extism_store_u8(offset: BigInt, n: number) {
             //@ts-ignore
-            plugin.memory[offset] = Number(n)
+            plugin.allocator.memory[offset - BigInt(1)] = Number(n)
         },
         extism_store_u32(n: BigInt, i: number) {debugger; },
         extism_store_u64(offset: BigInt, n: BigInt) {
-            let parts = unpackU64toU8(n)
-            parts.forEach((p, i) => {
-                plugin.memory[i + Number(offset)] = p
-            })
+            const tmp = new BigUint64Array(plugin.allocator.memory.buffer, Number(offset) - 1);
+            tmp[0] = n;
         },
         extism_input_length(): BigInt {
             //@ts-ignore
@@ -38,14 +96,14 @@ function makeEnv(plugin: ExtismPluginCall): any {
             //@ts-ignore
             return plugin.input[i]
         },
-        extism_input_load_u64(idx: BigInt) {
-            const i = Number(idx)
+        extism_input_load_u64(idx: BigInt) : BigInt {
             //@ts-ignore
-            return packU8toU64(plugin.input.slice(i, i+8))
+            let cast = new BigUint64Array(plugin.allocator.memory.buffer, Number(idx));
+            return cast[0];
         },
         extism_output_set(offset: BigInt, len: number): number {
             //@ts-ignore
-            offset = Number(offset)
+            offset = Number(offset) - 1
             len = Number(len)
             //@ts-ignore
             plugin.output = plugin.memory.slice(offset, offset+len)
@@ -56,7 +114,7 @@ function makeEnv(plugin: ExtismPluginCall): any {
         extism_var_get(i: BigInt): number {debugger;return 0},
         extism_var_set(n: BigInt, i: BigInt) {debugger;},
         extism_http_request(n: BigInt, i: BigInt): number {debugger;return 0},
-        extism_length(i: BigInt): number {debugger;return 0},
+        extism_length(i: BigInt): number { return plugin.allocator.getLength(i); },
         extism_log_warn(i: number) {debugger;},
         extism_log_info(i: number) {debugger;},
         extism_log_debug(i: number) {debugger;},
