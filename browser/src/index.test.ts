915b376bb51bf83698e9a9a0c619bae495df6eae
Benjamin Eckel <bhelx@simst.im>
2022-11-09 11:36:20 -0600

    test(browser-runtime): Add a simple test and runner

--- browser/src/index.test.ts
+++ browser/src/index.test.ts
@@ -1,6 +1,23 @@
-import { sum } from './';
-describe('sum function', () => {
-  it('adds 2 numbers', () => {
-    expect(sum(1, 2)).toBe(3);
+import ExtismContext from './'
+import fs from 'fs'
+import path from 'path'
+
+function parse(bytes: Uint8Array): any {
+  return JSON.parse(new TextDecoder().decode(bytes))
+}
+
+describe('', () => {
+  it('can load and call a plugin', async () => {
+    const data = fs.readFileSync(path.join(__dirname, "..", "data", "code.wasm"))
+    const ctx = new ExtismContext()
+    const plugin = await ctx.newPlugin({ "wasm": [{"data": data}] })
+    const functions = await plugin.getExportedFunctions()
+    expect(functions).toEqual(["count_vowels"])
+    let output = await plugin.call("count_vowels", "this is a test")
+    expect(parse(output)).toEqual({"count": 4})
+    output = await plugin.call("count_vowels", "this is a test again")
+    expect(parse(output)).toEqual({"count": 7})
+    output = await plugin.call("count_vowels", "this is a test thrice")
+    expect(parse(output)).toEqual({"count": 6})
   });
 });
