1b3f579d0def96343b39e3010f46d66b035a95ac
Benjamin Eckel <bhelx@simst.im>
2022-11-29 15:26:26 -0600

    docs(elixir-sdk): Reduce elixir min version

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -5,7 +5,7 @@ defmodule Extism.MixProject do
     [
       app: :extism,
       version: "0.0.1",
-      elixir: "~> 1.14",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
