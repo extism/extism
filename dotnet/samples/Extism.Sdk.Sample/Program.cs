c3ffb25891e1e404148fe4bb993d756ad5347f77
Muhammad Azeez <muhammad-azeez@outlook.com>
2022-12-08 21:42:27 +0300

    feat: Implement .NET Host SDK (#119)
    I am working on a .NET host for extism. My plan is to do the following:
     - [x] Implement a proof of concept to make sure things are possible
    - [x] Write docs for the C# API so that the users get a nice IDE
    experience
     - [x] Create a github action to publish the NuGet packages
        - [x] Edit `ci.yml` to include .NET Sdk
    - [x] Create `release-dotnet.yml` to release `Extism.Sdk` nuget package
    - [x] Maybe Create `release-dotnet-native.yml` to release
    `Extism.runtime.win` nuget package
     - [x] Test on Linux (Help needed)
     - [x] Test on Mac (Help needed)
     - [x] Expose all of the Extism functions
     - [x] Write automated tests
    - [x] ~Edit README show that the there is a .NET SDK~. Probably we
    should not do this until we have a docs page.
     - [x] ~Use the `Extism.runtime.win-x64` package in the sample project~
    
    Out of scope for this PR:
     - Json Serialization/Desererialization support
    
    Co-authored-by: Alistair Evans <alistairjevans@gmail.com>
    Co-authored-by: Benjamin Eckel <bhelx@simst.im>
    Co-authored-by: Benjamin Eckel <bhelx@users.noreply.github.com>

---
+++ dotnet/samples/Extism.Sdk.Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using Extism.Sdk.Native;
+
+using System.Reflection;
+using System.Text;
+
+Console.WriteLine($"Extism version {Context.GetExtismVersion()}");
+var x = Context.SetExtismLogFile("log.log", LogLevel.Trace);
+
+using var context = new Context();
+
+var binDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+var wasm = File.ReadAllBytes(Path.Combine(binDirectory, "code.wasm"));
+using var plugin = context.CreatePlugin(wasm, withWasi: true);
+
+Console.Write("Input: ");
+var input = Console.ReadLine() ?? "";
+
+var output = plugin.CallFunction("count_vowels", Encoding.UTF8.GetBytes(input));
+Console.WriteLine(Encoding.UTF8.GetString(output));
