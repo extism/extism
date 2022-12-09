cbd973fc21482ded333482cbed04701c23c20c0e
Thomas Darimont <thomas.darimont@googlemail.com>
2022-12-09 20:41:57 +0100

    (java-sdk) Revert max_pages back to max for MemoryOptions

--- java/src/main/java/org/extism/sdk/manifest/MemoryOptions.java
+++ java/src/main/java/org/extism/sdk/manifest/MemoryOptions.java
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName;
  * Configures memory for the Wasm runtime.
  * Memory is described in units of pages (64KB) and represent contiguous chunks of addressable memory.
  *
- * @param maxPages Max number of pages.
+ * @param max Max number of pages.
  */
-public record MemoryOptions(@SerializedName("max_pages") Integer maxPages) {
+public record MemoryOptions(@SerializedName("max") Integer max) {
 }
