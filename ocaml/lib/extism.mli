cebc6f0d3366d83b224093f1b80ccc0281ed439f
zach <zach@dylib.so>
2022-12-19 15:37:17 -0800

    doc: add some basic comments

--- ocaml/lib/extism.mli
+++ ocaml/lib/extism.mli
@@ -1,24 +1,38 @@
+(** Extism bindings for OCaml *)
+
+(** The error type used throughout this library *)
 type error = [ `Msg of string ]
 
+(** Returns the libextism version, not the version of the OCaml library *)
 val extism_version : unit -> string
 
+(** [Manifest] is used to configure, fetch and link WebAssembly modules *)
 module Manifest : sig
+
+  (** Memory options *)
   type memory = { max_pages : int option } [@@deriving yojson]
+
+  (** Key/value dictionary *)
   type dict = (string * string) list
+
+  (** Key/value dictionary with optional values *)
   type config = (string * string option) list
 
+  (** WebAssembly file *)
   type wasm_file = {
     path : string;
     name : string option; [@yojson.option]
     hash : string option; [@yojson.option]
   }
 
+  (** WebAssembly module data *)
   type wasm_data = {
     data : string;
     name : string option; [@yojson.option]
     hash : string option; [@yojson.option]
   }
 
+  (** WebAssembly URL *)
   type wasm_url = {
     url : string;
     headers : dict option; [@yojson.option]
@@ -27,8 +41,10 @@ module Manifest : sig
     hash : string option; [@yojson.option]
   }
 
+  (** WebAssembly from a file, module data or URL *)
   type wasm = File of wasm_file | Data of wasm_data | Url of wasm_url
 
+  (** Manifest type *)
   type t = {
     wasm : wasm list;
     memory : memory option;
@@ -38,9 +54,13 @@ module Manifest : sig
     timeout_ms : int option;
   }
 
+  (** Create [wasm] from filename *)
   val file : ?name:string -> ?hash:string -> string -> wasm
+
+  (** Create [wasm] from WebAssembly module data *)
   val data : ?name:string -> ?hash:string -> string -> wasm
 
+  (** Create [wasm] from URL *)
   val url :
     ?headers:(string * string) list ->
     ?name:string ->
@@ -49,6 +69,7 @@ module Manifest : sig
     string ->
     wasm
 
+  (** Create new manifest *)
   val v :
     ?config:config ->
     ?memory:memory ->
@@ -58,26 +79,40 @@ module Manifest : sig
     wasm list ->
     t
 
+  (** Convert manifest to JSON *)
   val json : t -> string
+
+  (** Updates a manifest config *)
   val with_config : t -> config -> t
 end
 
+(** [Context] is used to group plugins *)
 module Context : sig
+  (** Context type *)
   type t
 
+  (** Create a new context *)
   val create : unit -> t
+
+  (** Free a context. All plugins will be removed and the value should not be 
+      accessed after this call *)
   val free : t -> unit
+
+  (** Reset a context. All plugins will be removed *)
   val reset : t -> unit
 end
 
+(** Execute a function with a fresh context and free it after *)
 val with_context : (Context.t -> 'a) -> 'a
 
 val set_log_file :
   ?level:[ `Error | `Warn | `Info | `Debug | `Trace ] -> string -> bool
 
+(** [Plugins] contain functions that can be called *)
 module Plugin : sig
   type t
 
+  (** Make a new plugin from raw WebAssembly or JSON encoded manifest *)
   val make :
     ?config:Manifest.config ->
     ?wasi:bool ->
@@ -85,9 +120,11 @@ module Plugin : sig
     string ->
     (t, [ `Msg of string ]) result
 
+  (** Make a new plugin from a [Manifest] *)
   val of_manifest :
     ?wasi:bool -> Context.t -> Manifest.t -> (t, [ `Msg of string ]) result
 
+  (** Update a plugin from raw WebAssembly or JSON encoded manifest *)
   val update :
     t ->
     ?config:(string * string option) list ->
@@ -95,13 +132,20 @@ module Plugin : sig
     string ->
     (unit, [ `Msg of string ]) result
 
+  (** Update a plugin from a [Manifest] *)
   val update_manifest :
     t -> ?wasi:bool -> Manifest.t -> (unit, [ `Msg of string ]) result
 
+  (** Call a function, uses [Bigstringaf.t] for input/output *)
   val call_bigstring :
     t -> name:string -> Bigstringaf.t -> (Bigstringaf.t, error) result
 
+  (** Call a function, uses [string] for input/output *)
   val call : t -> name:string -> string -> (string, error) result
+
+  (** Drop a plugin *)
   val free : t -> unit
+
+  (** Check if a function is exported by a plugin *)
   val function_exists : t -> string -> bool
 end
