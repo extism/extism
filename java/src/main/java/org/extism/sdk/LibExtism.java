a6539a0f35b9eed15dbc08d70f91154781628233
zach <zach@dylib.so>
2022-12-19 15:14:25 -0800

    tmp: test editing file in java directory

--- java/src/main/java/org/extism/sdk/LibExtism.java
+++ java/src/main/java/org/extism/sdk/LibExtism.java
@@ -2,8 +2,8 @@ package org.extism.sdk;
 
 import com.sun.jna.Library;
 import com.sun.jna.Native;
-import com.sun.jna.Pointer;
 import com.sun.jna.ptr.IntByReference;
+import com.sun.jna.Pointer;
 
 /**
  * Wrapper around the Extism library.
