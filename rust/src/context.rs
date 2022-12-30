568c04b1ec326bc40363ae54a0f581073aa1d9ec
zach <zach@dylib.so>
2022-12-29 17:03:52 -0800

    cleanup: remove commented out code

--- rust/src/context.rs
+++ rust/src/context.rs
@@ -27,34 +27,4 @@ impl Context {
             Err(x) => x.into_inner(),
         }
     }
-
-    // pub fn plugin_memory(&mut self, mem: MemoryBlock) -> Option<*mut u8> {
-    //     match self.lock().current_plugin() {
-    //         Some(p) => unsafe { Some(p.memory.data_mut().as_mut_ptr().add(mem.offset)) },
-    //         None => None,
-    //     }
-    // }
-
-    // pub fn current_plugin_alloc(&mut self, n: usize) -> Result<MemoryBlock, Error> {
-    //     match self.lock().current_plugin() {
-    //         Some(p) => Ok(p.memory.alloc(n)?),
-    //         None => Err(Error::Message("No active plugin".into())),
-    //     }
-    // }
-
-    // pub fn current_plugin_free(&mut self, block: MemoryBlock) {
-    //     match self.lock().current_plugin() {
-    //         Some(p) => p.memory.free(block.offset),
-    //         None => (),
-    //     }
-    // }
-
-    // pub fn current_plugin_memory_block(&self, offs: usize) -> Option<MemoryBlock> {
-    //     self.lock().current_plugin().and_then(|plugin| {
-    //         plugin
-    //             .memory
-    //             .block_length(offs)
-    //             .and_then(|len| Some(MemoryBlock::new(offs, len)))
-    //     })
-    // }
 }
