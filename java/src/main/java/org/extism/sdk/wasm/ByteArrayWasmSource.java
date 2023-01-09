85da0843d2d7509c7c37b6ad67d85884ed5012ec
Etienne ANNE <etienne.anne@icloud.com>
2023-01-09 10:38:03 +0100

    support jdk11

--- java/src/main/java/org/extism/sdk/wasm/ByteArrayWasmSource.java
+++ java/src/main/java/org/extism/sdk/wasm/ByteArrayWasmSource.java
@@ -2,10 +2,35 @@ package org.extism.sdk.wasm;
 
 /**
  * WASM Source represented by raw bytes.
- *
- * @param name
- * @param data the byte array representing the WASM code
- * @param hash
  */
-public record ByteArrayWasmSource(String name, byte[] data, String hash) implements WasmSource {
+public class ByteArrayWasmSource implements WasmSource {
+
+    private String name;
+
+    private byte[] data;
+
+    private String hash;
+
+
+    /**
+     * Constructor
+     * @param name
+     * @param data the byte array representing the WASM code
+     * @param hash
+     */
+    public ByteArrayWasmSource(String name, byte[] data, String hash) {
+        this.name = name;
+        this.data = data;
+        this.hash = hash;
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public String hash() {
+        return hash;
+    }
 }
