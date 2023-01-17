1e39542b3267181a89a537ca17c9358ee8a9a075
zach <zach@dylib.so>
2023-01-17 10:04:08 -0800

    test(cpp): add test for host functions

--- cpp/test/test.cpp
+++ cpp/test/test.cpp
@@ -10,6 +10,8 @@ std::vector<uint8_t> read(const char *filename) {
                               std::istreambuf_iterator<char>());
 }
 
+const std::string code = "../../wasm/code.wasm";
+
 namespace {
 using namespace extism;
 
@@ -20,7 +22,7 @@ TEST(Context, Basic) {
 
 TEST(Plugin, Manifest) {
   Context context;
-  Manifest manifest = Manifest::path("code.wasm");
+  Manifest manifest = Manifest::path(code);
   manifest.set_config("a", "1");
 
   ASSERT_NO_THROW(Plugin plugin = context.plugin(manifest));
@@ -38,7 +40,7 @@ TEST(Plugin, BadManifest) {
 
 TEST(Plugin, Bytes) {
   Context context;
-  auto wasm = read("code.wasm");
+  auto wasm = read(code.c_str());
   ASSERT_NO_THROW(Plugin plugin = context.plugin(wasm));
   Plugin plugin = context.plugin(wasm);
 
@@ -48,7 +50,7 @@ TEST(Plugin, Bytes) {
 
 TEST(Plugin, UpdateConfig) {
   Context context;
-  auto wasm = read("code.wasm");
+  auto wasm = read(code.c_str());
   Plugin plugin = context.plugin(wasm);
 
   Config config;
@@ -58,11 +60,32 @@ TEST(Plugin, UpdateConfig) {
 
 TEST(Plugin, FunctionExists) {
   Context context;
-  auto wasm = read("code.wasm");
+  auto wasm = read(code.c_str());
   Plugin plugin = context.plugin(wasm);
 
   ASSERT_FALSE(plugin.function_exists("bad_function"));
   ASSERT_TRUE(plugin.function_exists("count_vowels"));
+}
+
+TEST(Plugin, HostFunction) {
+  Context context;
+  auto wasm = read("../../wasm/code-functions.wasm");
+  auto t = std::vector<ValType>{ValType::I64};
+  Function hello_world =
+      Function("hello_world", t, t,
+               [](CurrentPlugin plugin, const std::vector<Val> &params,
+                  std::vector<Val> &results, void *user_data) {
+                 auto offs = plugin.alloc(4);
+                 memcpy(plugin.memory() + offs, "test", 4);
+                 results[0].v.i64 = (int64_t)offs;
+               });
+  auto functions = std::vector<Function>{
+      hello_world,
+  };
+  Plugin plugin = context.plugin(wasm, true, functions);
+  auto buf = plugin.call("count_vowels", "aaa");
+  ASSERT_EQ(buf.length, 4);
+  ASSERT_EQ((std::string)buf, "test");
 }
 
 }; // namespace
