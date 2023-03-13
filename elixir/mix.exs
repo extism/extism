9c02f10e4a6afc506357f4fd35a2746689afca2f
Benjamin Eckel <bhelx@simst.im>
2023-03-13 11:29:03 -0500

    release: 0.3.0

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Extism.MixProject do
   def project do
     [
       app: :extism,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
