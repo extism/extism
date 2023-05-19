8fb06973cf25ebfd7a1e0d5fd6c0f670096567bf
Benjamin Eckel <bhelx@simst.im>
2023-05-19 15:27:28 -0500

    chore(elixir): Bump to 0.4.0

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Extism.MixProject do
   def project do
     [
       app: :extism,
-      version: "0.3.2",
+      version: "0.4.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
