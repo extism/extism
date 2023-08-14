49810e463ea8620f978947aca52d7e1c255d4751
zach <zach@dylibso.com>
2023-08-14 16:52:59 -0700

    refactor(elixir): remove context

--- elixir/lib/extism/native.ex
+++ elixir/lib/extism/native.ex
@@ -7,16 +7,12 @@ defmodule Extism.Native do
     otp_app: :extism,
     crate: :extism_nif
 
-  def context_new(), do: error()
-  def context_reset(_ctx), do: error()
-  def context_free(_ctx), do: error()
-  def plugin_new_with_manifest(_ctx, _manifest, _wasi), do: error()
-  def plugin_call(_ctx, _plugin_id, _name, _input), do: error()
-  def plugin_update_manifest(_ctx, _plugin_id, _manifest, _wasi), do: error()
-  def plugin_has_function(_ctx, _plugin_id, _function_name), do: error()
-  def plugin_free(_ctx, _plugin_id), do: error()
+  def plugin_new_with_manifest(_manifest, _wasi), do: error()
+  def plugin_call(_plugin, _name, _input), do: error()
+  def plugin_has_function(_plugin, _function_name), do: error()
+  def plugin_free(_plugin), do: error()
   def set_log_file(_filename, _level), do: error()
-  def plugin_cancel_handle(_ctx, _plugin_id), do: error()
+  def plugin_cancel_handle(_plugin), do: error()
   def plugin_cancel(_handle), do: error()
 
   defp error, do: :erlang.nif_error(:nif_not_loaded)
--- elixir/lib/extism/plugin.ex
+++ elixir/lib/extism/plugin.ex
@@ -3,28 +3,25 @@ defmodule Extism.Plugin do
   A Plugin represents an instance of your WASM program from the given manifest.
   """
   defstruct [
-    # The actual NIF Resource. PluginIndex and the context
-    plugin_id: nil,
-    ctx: nil
+    # The actual NIF Resource
+    plugin: nil,
   ]
 
-  def wrap_resource(ctx, plugin_id) do
+  def wrap_resource(plugin) do
     %__MODULE__{
-      ctx: ctx,
-      plugin_id: plugin_id
+      plugin: plugin
     }
   end
 
   @doc """
   Creates a new plugin
   """
-  def new(manifest, wasi \\ false, context \\ nil) do
-    ctx = context || Extism.Context.new()
+  def new(manifest, wasi \\ false) do
     {:ok, manifest_payload} = JSON.encode(manifest)
 
-    case Extism.Native.plugin_new_with_manifest(ctx.ptr, manifest_payload, wasi) do
+    case Extism.Native.plugin_new_with_manifest(manifest_payload, wasi) do
       {:error, err} -> {:error, err}
-      res -> {:ok, Extism.Plugin.wrap_resource(ctx, res)}
+      res -> {:ok, Extism.Plugin.wrap_resource(res)}
     end
   end
 
@@ -49,34 +46,9 @@ defmodule Extism.Plugin do
 
   """
   def call(plugin, name, input) do
-    case Extism.Native.plugin_call(plugin.ctx.ptr, plugin.plugin_id, name, input) do
+    case Extism.Native.plugin_call(plugin.plugin, name, input) do
       {:error, err} -> {:error, err}
       res -> {:ok, res}
-    end
-  end
-
-  @doc """
-  Updates the manifest of the given plugin
-
-  ## Parameters
-
-    - ctx: The Context to manage this plugin
-    - manifest: The String or Map of the WASM module or [manifest](https://extism.org/docs/concepts/manifest)
-    - wasi: A bool you set to true if you want WASI support
-
-
-  """
-  def update(plugin, manifest, wasi) when is_map(manifest) do
-    {:ok, manifest_payload} = JSON.encode(manifest)
-
-    case Extism.Native.plugin_update_manifest(
-           plugin.ctx.ptr,
-           plugin.plugin_id,
-           manifest_payload,
-           wasi
-         ) do
-      {:error, err} -> {:error, err}
-      _ -> :ok
     end
   end
 
@@ -84,14 +56,14 @@ defmodule Extism.Plugin do
   Frees the plugin
   """
   def free(plugin) do
-    Extism.Native.plugin_free(plugin.ctx.ptr, plugin.plugin_id)
+    Extism.Native.plugin_free(plugin.plugin)
   end
 
   @doc """
   Returns true if the given plugin responds to the given function name
   """
   def has_function(plugin, function_name) do
-    Extism.Native.plugin_has_function(plugin.ctx.ptr, plugin.plugin_id, function_name)
+    Extism.Native.plugin_has_function(plugin.plugin, function_name)
   end
 end
 
@@ -99,6 +71,6 @@ defimpl Inspect, for: Extim.Plugin do
   import Inspect.Algebra
 
   def inspect(dict, opts) do
-    concat(["#Extism.Plugin<", to_doc(dict.plugin_id, opts), ">"])
+    concat(["#Extism.Plugin<", to_doc(dict.plugin, opts), ">"])
   end
 end
