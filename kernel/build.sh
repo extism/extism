907530a4aa4d8db6114e80590d39a517d95ac218
zach <zach@dylibso.com>
2023-08-23 15:43:28 -0700

    cleanup: require wasm-strip in kernel build.sh now that it's only called manually

--- kernel/build.sh
+++ kernel/build.sh
@@ -2,6 +2,6 @@
 
 cargo build --release --target wasm32-unknown-unknown --package extism-runtime-kernel --bin extism-runtime
 cp target/wasm32-unknown-unknown/release/extism-runtime.wasm .
-wasm-strip extism-runtime.wasm || :
+wasm-strip extism-runtime.wasm
 mv extism-runtime.wasm ../runtime/src/extism-runtime.wasm
 
