1a0af37edf4a68e5e0105a32d097232a66070ded
Chris Dickinson <chris@dylibso.com>
2023-11-28 16:51:59 -0800

    doc: add example of linking modules in a plugin

--- runtime/benches/bench.rs
+++ runtime/benches/bench.rs
@@ -65,17 +65,7 @@ pub fn consume(c: &mut Criterion) {
     g.sample_size(500);
     g.noise_threshold(1.0);
     g.significance_level(0.2);
-    let mut plugin = PluginBuilder::new(CONSUME)
-        .with_wasi(true)
-        .with_function(
-            "host_reflect",
-            [PTR],
-            [PTR],
-            UserData::default(),
-            hello_world,
-        )
-        .build()
-        .unwrap();
+    let mut plugin = PluginBuilder::new(CONSUME).build().unwrap();
 
     for (i, elements) in [
         b"a".repeat(65536),
@@ -103,17 +93,7 @@ pub fn echo(c: &mut Criterion) {
     g.sample_size(500);
     g.noise_threshold(1.0);
     g.significance_level(0.2);
-    let mut plugin = PluginBuilder::new(ECHO)
-        .with_wasi(true)
-        .with_function(
-            "host_reflect",
-            [PTR],
-            [PTR],
-            UserData::default(),
-            hello_world,
-        )
-        .build()
-        .unwrap();
+    let mut plugin = PluginBuilder::new(ECHO).build().unwrap();
 
     for (i, elements) in [
         b"a".repeat(65536),
@@ -174,11 +154,102 @@ pub fn reflect(c: &mut Criterion) {
     }
 }
 
+// This is an apples-to-apples comparison of a linked wasm "reflect" function to our host "reflect"
+// function.
+pub fn reflect_linked(c: &mut Criterion) {
+    let mut g = c.benchmark_group("reflect");
+    g.sample_size(500);
+    g.noise_threshold(1.0);
+    g.significance_level(0.2);
+    let manifest = Manifest::new([
+        Wasm::Data {
+            data: br#"(module
+    (import "extism:host/env" "length" (func $length (param i64) (result i64)))
+    (import "extism:host/env" "load_u64" (func $load_u64 (param i64) (result i64)))
+    (import "extism:host/env" "load_u8" (func $load_u8 (param i64) (result i32)))
+    (import "extism:host/env" "store_u64" (func $store_u64 (param i64 i64)))
+    (import "extism:host/env" "store_u8" (func $store_u8 (param i64 i32)))
+
+    (func (export "host_reflect") (param $extism_offset i64) (result i64)
+        (local $len i64)
+        (local $offset i64)
+        (local $len64 i64)
+
+        (local.set $offset (i64.const 0))
+        (local.set $len (call $length (local.get $extism_offset)))
+
+        (local.set $len64 (i64.shl (i64.shr_u (local.get $len) (i64.const 16)) (i64.const 16)))
+
+        (loop $to_upper
+            (call $store_u64
+              (i64.add (local.get $extism_offset) (local.get $offset))
+              (call $load_u64 (i64.add (local.get $extism_offset) (local.get $offset)))
+            )
+
+            (local.set $offset (i64.add (local.get $offset) (i64.const 8)))
+            (br_if $to_upper (i64.lt_u (local.get $offset) (local.get $len64)))
+        )
+
+        (if (i64.ne (local.get $len64) (local.get $len)) (then
+          (loop $to_upper
+              (call $store_u8
+                (i64.add (local.get $extism_offset) (local.get $offset))
+                (call $load_u8 (i64.add (local.get $extism_offset) (local.get $offset)))
+              )
+
+              (local.set $offset (i64.add (i64.const 1) (local.get $offset)))
+              (br_if $to_upper (i64.lt_u (local.get $offset) (local.get $len)))
+          )
+        ))
+        local.get $extism_offset
+    )
+)"#
+            .to_vec(),
+            meta: WasmMetadata {
+                name: Some("extism:host/user".to_string()),
+                hash: None,
+            },
+        },
+        Wasm::Data {
+            data: REFLECT.to_vec(),
+            meta: WasmMetadata {
+                name: Some("main".to_string()),
+                hash: None,
+            },
+        },
+    ]);
+    let mut plugin = PluginBuilder::new(manifest)
+        .with_wasi(true)
+        .build()
+        .unwrap();
+
+    for (i, elements) in [
+        b"a".repeat(65536),
+        b"a".repeat(65536 * 10),
+        b"a".repeat(65536 * 100),
+    ]
+    .iter()
+    .enumerate()
+    {
+        g.throughput(criterion::Throughput::Bytes(elements.len() as u64));
+        g.bench_with_input(
+            format!("reflect_linked {} bytes", 10u32.pow(i as u32) * 65536),
+            elements,
+            |b, elems| {
+                b.iter(|| {
+                    assert_eq!(elems, plugin.call::<_, &[u8]>("reflect", &elems).unwrap());
+                });
+            },
+        );
+    }
+}
+
 criterion_group!(
     benches,
     consume,
     echo,
     reflect,
+    reflect_linked,
     basic,
     create_plugin,
     count_vowels
