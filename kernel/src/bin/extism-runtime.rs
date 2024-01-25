38e6476797169be8ce9dcbdf95d5d671ab08209f
zach <zach@dylibso.com>
2024-01-24 17:35:56 -0800

    wip

--- kernel/src/bin/extism-runtime.rs
+++ kernel/src/bin/extism-runtime.rs
@@ -1,5 +1,5 @@
-#![no_main]
 #![no_std]
+#![no_main]
 
 pub use extism_runtime_kernel::*;
 
@@ -7,4 +7,15 @@ pub use extism_runtime_kernel::*;
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     core::arch::wasm32::unreachable()
+}
+
+#[no_mangle]
+pub unsafe fn _start() {
+    // Check that no length is valid when nothing has been allocated
+    reset();
+    let x = i64_symbol();
+    let a = length(x as u64);
+    if a != 0 {
+        core::arch::wasm32::unreachable()
+    }
 }
