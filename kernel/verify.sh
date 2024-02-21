f41bd8b893f72ee4c3d6c9c35ac44300d26e8268
zach <zach@dylibso.com>
2024-02-21 11:30:52 -0800

    cleanup: ignore verification artifacts

---
+++ kernel/verify.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cargo build --release --target wasm32-unknown-unknown --features proof
+wasm2wat ./target/wasm32-unknown-unknown/release/extism-runtime.wasm > ./extism-runtime.proof.wat
+owi sym ./extism-runtime.proof.wat $@
