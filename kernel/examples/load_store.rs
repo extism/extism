a62e37c21b3989e406e36a66dd85ad9521c697a6
zach <zach@dylibso.com>
2024-02-23 14:52:24 -0800

    cleanup: reduce the size of the allocation used in load_store example

--- kernel/examples/load_store.rs
+++ kernel/examples/load_store.rs
@@ -7,15 +7,12 @@ main!({
     reset();
 
     let x = u64();
-    assume(x < i32::MAX as u64);
+    assume(x < 64);
     assume(x > 0);
 
-    let offs = u64();
-    assume(offs <= x);
-    assume(offs > 0);
-
     let m = alloc(x);
-    for i in 0..m {
+    assert(length(m) == x);
+    for i in 0..x {
         store_u8(m + i, i as u8);
         assert(load_u8(m + i) == i as u8);
     }
