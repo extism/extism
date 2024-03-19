8241b959ae8fb8b990a0be78b3f2113e8233d842
zach <zach@dylibso.com>
2024-03-18 17:07:38 -0700

    refactor(pool): use a single lock for Pool.inner to avoid too many instances being created

--- runtime/src/pool.rs
+++ runtime/src/pool.rs
@@ -1,5 +1,6 @@
+use std::collections::HashMap;
+
 use crate::{Error, FromBytesOwned, Plugin, PluginBuilder, ToBytes};
-use dashmap::DashMap;
 
 /// `PoolPlugin` is used by the pool to track the number of live instances of a particular plugin
 #[derive(Clone, Debug)]
@@ -33,15 +34,15 @@ impl PoolPlugin {
 type PluginSource = dyn Fn() -> Result<Plugin, Error>;
 
 struct PoolInner<Key: std::fmt::Debug + Clone + std::hash::Hash + Eq = String> {
-    plugins: DashMap<Key, Box<PluginSource>>,
-    instances: DashMap<Key, Vec<PoolPlugin>>,
+    plugins: HashMap<Key, Box<PluginSource>>,
+    instances: HashMap<Key, Vec<PoolPlugin>>,
 }
 
 /// `Pool` manages threadsafe access to a limited number of instances of multiple plugins
 #[derive(Clone)]
 pub struct Pool<Key: std::fmt::Debug + Clone + std::hash::Hash + Eq = String> {
     max_instances: usize,
-    inner: std::sync::Arc<PoolInner<Key>>,
+    inner: std::sync::Arc<std::sync::Mutex<PoolInner<Key>>>,
 }
 
 unsafe impl<T: std::fmt::Debug + Clone + std::hash::Hash + Eq> Send for Pool<T> {}
@@ -52,10 +53,10 @@ impl<Key: std::fmt::Debug + Clone + std::hash::Hash + Eq> Pool<Key> {
     pub fn new(max_instances: usize) -> Self {
         Pool {
             max_instances,
-            inner: std::sync::Arc::new(PoolInner {
+            inner: std::sync::Arc::new(std::sync::Mutex::new(PoolInner {
                 plugins: Default::default(),
                 instances: Default::default(),
-            }),
+            })),
         }
     }
 
@@ -64,7 +65,7 @@ impl<Key: std::fmt::Debug + Clone + std::hash::Hash + Eq> Pool<Key> {
     where
         F: 'static,
     {
-        let pool = &self.inner;
+        let mut pool = self.inner.lock().unwrap();
         if !pool.instances.contains_key(&key) {
             pool.instances.insert(key.clone(), vec![]);
         }
@@ -74,7 +75,7 @@ impl<Key: std::fmt::Debug + Clone + std::hash::Hash + Eq> Pool<Key> {
 
     /// Add a plugin using a `PluginBuilder`
     pub fn add_builder(&self, key: Key, source: PluginBuilder<'static>) {
-        let pool = &self.inner;
+        let mut pool = self.inner.lock().unwrap();
         if !pool.instances.contains_key(&key) {
             pool.instances.insert(key.clone(), vec![]);
         }
@@ -84,7 +85,8 @@ impl<Key: std::fmt::Debug + Clone + std::hash::Hash + Eq> Pool<Key> {
     }
 
     fn find_available(&self, key: &Key) -> Result<Option<PoolPlugin>, Error> {
-        if let Some(entry) = self.inner.instances.get_mut(key) {
+        let mut pool = self.inner.lock().unwrap();
+        if let Some(entry) = pool.instances.get_mut(key) {
             for instance in entry.iter() {
                 if std::rc::Rc::strong_count(&instance.0) == 1 {
                     return Ok(Some(instance.clone()));
@@ -97,6 +99,8 @@ impl<Key: std::fmt::Debug + Clone + std::hash::Hash + Eq> Pool<Key> {
     /// Get the number of live instances for a plugin
     pub fn count(&self, key: &Key) -> usize {
         self.inner
+            .lock()
+            .unwrap()
             .instances
             .get(key)
             .map(|x| x.len())
@@ -117,13 +121,16 @@ impl<Key: std::fmt::Debug + Clone + std::hash::Hash + Eq> Pool<Key> {
             return Ok(Some(avail));
         }
 
-        if self.inner.instances.get(key).map(|x| x.len()).unwrap_or(0) < max {
-            if let Some(source) = self.inner.plugins.get(key) {
-                let plugin = source()?;
-                let instance = PoolPlugin::new(plugin);
-                let mut v = self.inner.instances.get_mut(key).unwrap();
-                v.push(instance);
-                return Ok(Some(v.last().unwrap().clone()));
+        {
+            let mut pool = self.inner.lock().unwrap();
+            if pool.instances.get(key).map(|x| x.len()).unwrap_or_default() < max {
+                if let Some(source) = pool.plugins.get(key) {
+                    let plugin = source()?;
+                    let instance = PoolPlugin::new(plugin);
+                    let v = pool.instances.get_mut(key).unwrap();
+                    v.push(instance);
+                    return Ok(Some(v.last().unwrap().clone()));
+                }
             }
         }
 
