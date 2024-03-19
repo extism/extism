4b59af72d4fbe3e8e6c8c9273d0bac8d5f832578
zach <zach@dylibso.com>
2024-03-18 17:07:38 -0700

    refactor(pool): use a single lock for Pool.inner to avoid too many instances being created

--- runtime/src/tests/pool.rs
+++ runtime/src/tests/pool.rs
@@ -4,7 +4,7 @@ fn run_thread(p: Pool<String>, i: u64) -> std::thread::JoinHandle<()> {
     std::thread::spawn(move || {
         std::thread::sleep(std::time::Duration::from_millis(i));
         let s: String = p
-            .get(&"test".to_string(), std::time::Duration::from_secs(5))
+            .get(&"test".to_string(), std::time::Duration::from_secs(1))
             .unwrap()
             .unwrap()
             .call("count_vowels", "abc")
@@ -15,24 +15,34 @@ fn run_thread(p: Pool<String>, i: u64) -> std::thread::JoinHandle<()> {
 
 #[test]
 fn test_threads() {
-    let data = include_bytes!("../../../wasm/code.wasm");
-    let pool: Pool<String> = Pool::new(2);
+    for i in 1..=3 {
+        let data = include_bytes!("../../../wasm/code.wasm");
+        let pool: Pool<String> = Pool::new(i);
 
-    let test = "test".to_string();
-    pool.add_builder(
-        test.clone(),
-        extism::PluginBuilder::new(extism::Manifest::new([extism::Wasm::data(data)]))
-            .with_wasi(true),
-    );
+        let test = "test".to_string();
+        pool.add_builder(
+            test.clone(),
+            extism::PluginBuilder::new(extism::Manifest::new([extism::Wasm::data(data)]))
+                .with_wasi(true),
+        );
 
-    let mut threads = vec![];
-    threads.push(run_thread(pool.clone(), 1000));
-    threads.push(run_thread(pool.clone(), 1000));
-    threads.push(run_thread(pool.clone(), 500));
-    threads.push(run_thread(pool.clone(), 0));
+        let mut threads = vec![];
+        threads.push(run_thread(pool.clone(), 1000));
+        threads.push(run_thread(pool.clone(), 1000));
+        threads.push(run_thread(pool.clone(), 1000));
+        threads.push(run_thread(pool.clone(), 1000));
+        threads.push(run_thread(pool.clone(), 1000));
+        threads.push(run_thread(pool.clone(), 1000));
+        threads.push(run_thread(pool.clone(), 500));
+        threads.push(run_thread(pool.clone(), 500));
+        threads.push(run_thread(pool.clone(), 500));
+        threads.push(run_thread(pool.clone(), 500));
+        threads.push(run_thread(pool.clone(), 500));
+        threads.push(run_thread(pool.clone(), 0));
 
-    for t in threads {
-        t.join().unwrap();
+        for t in threads {
+            t.join().unwrap();
+        }
+        assert!(pool.count(&test) <= i);
     }
-    assert_eq!(pool.count(&test), 2);
 }
