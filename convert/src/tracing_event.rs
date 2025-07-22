4f2b29b05b339547b5fb2475e725e904a7e42e80
Miles Johnson <mileswjohnson@gmail.com>
2025-07-22 15:00:46 -0700

    Add impl.

--- convert/src/tracing_event.rs
+++ convert/src/tracing_event.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::str::FromStr;
 
 #[derive(Default, Deserialize, Serialize)]
 #[serde(default)]
@@ -16,4 +17,44 @@ pub struct TracingEvent {
     /// https://docs.rs/tracing/latest/tracing/#configuring-attributes
     #[serde(skip_serializing_if = "Option::is_none")]
     pub target: Option<String>,
+}
+
+impl TracingEvent {
+    pub fn new(message: impl AsRef<str>) -> Self {
+        Self {
+            message: message.as_ref().into(),
+            ..Default::default()
+        }
+    }
+
+    pub fn field(&mut self, key: impl AsRef<str>, value: impl AsRef<str>) -> &mut Self {
+        self.fields
+            .insert(key.as_ref().into(), value.as_ref().into());
+        self
+    }
+
+    pub fn target(&mut self, value: impl AsRef<str>) -> &mut Self {
+        self.target = Some(value.as_ref().into());
+        self
+    }
+}
+
+impl FromStr for TracingEvent {
+    type Err = ();
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        Ok(Self::new(value))
+    }
+}
+
+impl From<&str> for TracingEvent {
+    fn from(value: &str) -> Self {
+        Self::new(value)
+    }
+}
+
+impl From<String> for TracingEvent {
+    fn from(value: String) -> Self {
+        Self::new(value)
+    }
 }
